d56188c272a928ae586fcabc7ca659dac2062205
Zeljko Lucic <zeljko.lucic99@gmail.com>
2022-10-13 14:31:38 +0200

    Create `FeedItem`

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,16 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Zeljko Lucic on 13.10.22..
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
+
