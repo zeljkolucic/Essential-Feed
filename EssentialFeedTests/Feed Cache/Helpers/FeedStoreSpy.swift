12cfe80c11f498e663fb4b4518e31c545f798640
Zeljko Lucic <zeljko.lucic99@gmail.com>
2022-10-26 13:19:49 +0200

    Load command delivers no images on empty cache

--- EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -56,7 +56,7 @@ class FeedStoreSpy: FeedStore {
         retrievalCompletions[index](error)
     }
     
-    func completeRetrievalSuccessfully(at index: Int = 0) {
+    func completeRetrievalWithEmptyCache(at index: Int = 0) {
         retrievalCompletions[index](nil)
     }
 }
--- EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/LoadFeedFromCacheUseCaseTests.swift
@@ -47,6 +47,28 @@ class LoadFeedFromCacheUseCaseTests: XCTestCase {
         XCTAssertEqual(receivedError as? NSError, retrievalError)
     }
     
+    func test_load_deliversNoImagesEmptyCache() {
+        let (sut, store) = makeSUT()
+
+        let expectation = expectation(description: "Wait for load completion...")
+
+        var receivedImages: [FeedImage]?
+        sut.load { result in
+            switch result {
+            case let .success(images):
+                receivedImages = images
+            default:
+                XCTFail("Expected success, got \(result) instead.")
+            }
+            expectation.fulfill()
+        }
+
+        store.completeRetrievalWithEmptyCache()
+        wait(for: [expectation], timeout: 1.0)
+
+        XCTAssertEqual(receivedImages, [])
+    }
+    
     // MARK: - Helpers
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #filePath, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
