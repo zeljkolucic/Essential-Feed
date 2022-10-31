d0269969b0546503303361d99192f2af7a08b576
Zeljko Lucic <zeljko.lucic99@gmail.com>
2022-10-31 22:29:51 +0100

    Add fade-in animation to simulate asynchronous image loading

--- Prototype/Prototype/FeedImageCell.swift
+++ Prototype/Prototype/FeedImageCell.swift
@@ -12,4 +12,24 @@ final class FeedImageCell: UITableViewCell {
     @IBOutlet weak var locationLabel: UILabel!
     @IBOutlet weak var feedImageView: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+        feedImageView.alpha = 0
+    }
+    
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        
+        feedImageView.alpha = 0
+    }
+    
+    func fadeIn(_ image: UIImage?) {
+        feedImageView.image = image
+        
+        UIView.animate(withDuration: 0.3, delay: 0.3, options: []) {
+            self.feedImageView.alpha = 1
+        }
+    }
 }
