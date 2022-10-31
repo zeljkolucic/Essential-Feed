790c5f3142d99e32fe719aada3b432712ca7c5a8
Zeljko Lucic <zeljko.lucic99@gmail.com>
2022-10-31 22:26:31 +0100

    Render prototype feed date on the table view

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Zeljko Lucic on 31.10.22..
+//
+
+import UIKit
+
+final class FeedImageCell: UITableViewCell {
+    @IBOutlet weak var locationContainer: UIStackView!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var feedImageView: UIImageView!
+    @IBOutlet weak var descriptionLabel: UILabel!
+}
