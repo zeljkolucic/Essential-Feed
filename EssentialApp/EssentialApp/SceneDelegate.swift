3008488104d8963711df013ed543d1dedbd7fb0c
Zeljko Lucic <zeljko.lucic99@gmail.com>
2023-01-25 12:28:53 +0100

    Compose `RemoteFeedLoader` and `LocalFeedLoader` within `FeedLoaderCacheDecorator` in `SceneDelegate`

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -31,7 +31,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         let feedViewController = FeedUIComposer.feedComposedWith(
             feedLoader: FeedLoaderWithFallbackComposite(
-                primary: remoteFeedLoader,
+                primary: FeedLoaderCacheDecorator(
+                    decoratee: remoteFeedLoader,
+                    cache: localFeedLoader),
                 fallback: localFeedLoader),
             imageLoader: FeedImageDataLoaderWithFallbackComposite(
                 primary: localImageLoader,
