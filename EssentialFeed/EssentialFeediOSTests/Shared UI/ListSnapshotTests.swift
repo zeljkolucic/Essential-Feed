87ee4888cd6d44f980d8fe2ac53e19b6d8b3f4b7
Zeljko Lucic <zeljko.lucic99@gmail.com>
2023-02-05 21:17:03 +0100

    Implement `ImageComments` UI

--- EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
+++ EssentialFeed/EssentialFeediOSTests/Shared UI/ListSnapshotTests.swift
@@ -33,7 +33,7 @@ class ListSnapshotTests: XCTestCase {
     
     private func makeSUT() -> ListViewController {
         let bundle = Bundle(for: ListViewController.self)
-        let storyboard = UIStoryboard(name: "Feed", bundle: bundle)
+        let storyboard = UIStoryboard(name: "ImageComments", bundle: bundle)
         let controller = storyboard.instantiateInitialViewController() as! ListViewController
         controller.loadViewIfNeeded()
         controller.tableView.showsVerticalScrollIndicator = false
