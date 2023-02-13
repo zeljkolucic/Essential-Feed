85a4b8f62800ce2f13c99268ce9bf7d0ca6f588d
Zeljko Lucic <zeljko.lucic99@gmail.com>
2023-02-13 10:16:45 +0100

    Set `FeedEndpoint` limit URLQueryItem to 10

---
+++ EssentialFeed/EssentialFeed/Feed API/FeedEndpoint.swift
@@ -0,0 +1,26 @@
+//
+//  FeedEndpoint.swift
+//  EssentialFeed
+//
+//  Created by Zeljko Lucic on 13.2.23..
+//
+
+import Foundation
+
+public enum FeedEndpoint {
+    case get
+    
+    public func url(baseURL: URL) -> URL {
+        switch self {
+        case .get:
+            var components = URLComponents()
+            components.scheme = baseURL.scheme
+            components.host = baseURL.host
+            components.path = baseURL.path + "/v1/feed"
+            components.queryItems = [
+                URLQueryItem(name: "limit", value: "10")
+            ]
+            return components.url!
+        }
+    }
+}
---
+++ EssentialFeed/EssentialFeedTests/Feed API/FeedEndpointTests.swift
@@ -0,0 +1,24 @@
+//
+//  FeedEndpointTests.swift
+//  EssentialFeedTests
+//
+//  Created by Zeljko Lucic on 13.2.23..
+//
+
+import XCTest
+import EssentialFeed
+
+class FeedEndpointTests: XCTestCase {
+    
+    func test_feed_endpointURL() {
+        let baseURL = URL(string: "http://base-url.com")!
+        
+        let received = FeedEndpoint.get.url(baseURL: baseURL)
+        
+        XCTAssertEqual(received.scheme, "http", "scheme")
+        XCTAssertEqual(received.host, "base-url.com", "host")
+        XCTAssertEqual(received.path, "/v1/feed", "path")
+        XCTAssertEqual(received.query, "limit=10", "query")
+    }
+    
+}
