f412c7274cd0406616c4c60b5319ff286b1ef7fd
Zeljko Lucic <zeljko.lucic99@gmail.com>
2023-02-14 18:07:52 +0100

    Prevent loading resources again until a running request completes

--- EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
+++ EssentialApp/EssentialApp/LoadResourcePresentationAdapter.swift
@@ -12,6 +12,8 @@ import EssentialFeediOS
 final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     private let feedLoader: () -> AnyPublisher<Resource, Error>
     private var cancellable: Cancellable?
+    private var isLoading = false
+    
     var presenter: LoadResourcePresenter<Resource, View>?
     
     init(loader: @escaping () -> AnyPublisher<Resource, Error>) {
@@ -19,10 +21,16 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
     }
     
     func loadResource() {
+        guard !isLoading else { return }
+        
         presenter?.didStartLoading()
+        isLoading = true
         
         cancellable = feedLoader()
             .dispatchOnMainQueue()
+            .handleEvents(receiveCancel: { [weak self] in
+                self?.isLoading = false
+            })
             .sink(
             receiveCompletion: { [weak self] completion in
                 switch completion {
@@ -31,6 +39,8 @@ final class LoadResourcePresentationAdapter<Resource, View: ResourceView> {
                 case let .failure(error):
                     self?.presenter?.didFinishLoading(with: error)
                 }
+                
+                self?.isLoading = false
             }, receiveValue: { [weak self] feed in
                 self?.presenter?.didFinishLoading(with: feed)
             })
