fac7e93d714be10c866928ef48225e041d680beb
Zeljko Lucic <zeljko.lucic99@gmail.com>
2023-02-15 10:37:24 +0100

    Make `FeedImageDataLoader` synchrounous

--- EssentialFeed/EssentialFeed/Feed Feature/FeedImageDataLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Feature/FeedImageDataLoader.swift
@@ -12,7 +12,5 @@ public protocol FeedImageDataLoaderTask {
 }
 
 public protocol FeedImageDataLoader {
-    typealias Result = Swift.Result<Data, Error>
-    
-    func loadImageData(from url: URL, completion: @escaping (Result) -> Void) -> FeedImageDataLoaderTask
+    func loadImageData(from url: URL) throws -> Data
 }
